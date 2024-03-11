903aff3e980854df3e91dbe97e8c2ad9ebd0ddc8
BableshAAzad <bableshaazad@outlook.com>
2024-03-11 23:03:27 +0530

    first comment insta clone

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Router from './Pages/Router/Router'
+
+function App() {
+  return (
+    <div>
+      <Router />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+    <div>
+      <div>
+        <div>
+          <img className='w-40' src="https://i.imgur.com/zqpwkLQ.png" alt="logo" srcset="" />
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
